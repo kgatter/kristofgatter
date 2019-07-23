dd73aaf54319aa44370f0e4ed812b552e208128c
Kristof Gatter <mail@kristofgatter.com>
2019-07-23 20:18:57 +0200

    (KGButton) Create Custom Button Component

---
+++ src/Components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Button.module.scss';
+import classNames from 'classnames';
+import {Button} from 'reactstrap';
+
+export class KGButton extends React.Component {
+
+  render() {
+
+    const buttonClasses = classNames(
+        styles.Button,
+        this.props.className,
+        {
+        [styles.Button_green]: this.props.green,
+        [styles.Button_purple]: this.props.purple,
+        [styles.Button_sand]: this.props.sand
+        }
+    );
+
+    return (
+        <Button
+           className={buttonClasses}>
+           {this.props.children}
+        </Button>
+      )
+    }
+}
+
+export default KGButton;
