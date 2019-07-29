a8c1f12020cea915b8c0e6f81e415b7d1c761020
Kristof Gatter <mail@kristofgatter.com>
2019-07-29 19:26:25 +0200

    (WorkPage) Add Modulor

--- src/Pages/WorkPage.js
+++ src/Pages/WorkPage.js
@@ -11,6 +11,7 @@ import ImageAtlantisKY1 from "../Images/Work/AtlantisKY1.png";
 import ImageSI from "../Images/Work/SI.png";
 import VideoAvec from "../Images/Work/avecapp.mp4";
 import ImageAvec from "../Images/Work/avecapp.png";
+import ImageModulor from "../Images/Work/modulorMobile.png";
 
 function WorkPage() {
 
@@ -21,11 +22,14 @@ function WorkPage() {
     button1 = "Get in Touch",
     button2 = "Request a Portfolio",
     atlantisH1 = "Atlantis KY1 (2019)",
-    atlantisP1 = `Atlantis KY1 is a productivity software for machine workers and process engineers, created by Hackerbay, built on their Atlantis platform. I was the principal designer on the project. I facilitated several workshops with the CEO, the CTO and project management, helping to transform the vision of the product into an actual working MVP in less than three months. \n\nBesides the theoretical and UX/UI work, I've been involved with creating branding assets, copy, as well writing code for the UI.`,
+    atlantisP1 = `Atlantis KY1 is a productivity software for machine workers and process engineers, created by Hackerbay, built on their Atlantis platform. I was the principal designer on the project. I facilitated several workshops with the CEO, the CTO and project management, helping to transform the vision of the product into an actual working MVP in less than three months. \n\nBesides the theoretical and UX/UI work, I've been involved with creating branding assets, presentations, as well as writing code for the UI.`,
     siH1 = "Small Improvements (2015-2016)",
     siP1 = `Small Improvements is a performance management tool, offered as a SaaS solution to mid-sized companies. During my involvement with the company, I have worked as a UX Designer and a UI Developer. As part of one of three development teams, I was responsible for the UX rework of several key features like "Cycle Creation", and the flagship feature "360 Feedback". \n\nI was also involved with transitioning the app from an old-school static layout to a responsive one and had the opportunity to work with angularJS and React. After joining as a strong advocate of user-centered design principles, user tests became a regular occurrence, despite the company having done only one of them in its 4-year existence altogether before I had joined.`,
     valoraH1 = "Avec App (2019)",
-    valoraP1="Avec is a convenience store brand created by Valora, a Swiss retail holding company operating mainly in German-speaking countries. Working with the Hackerbay development team, we created a mobile app for Avec X, the experimental, cashierless flagship store of the brand located in Zurich's Central Station.\n\nThe app enables a fully cashierless shopping experience. Customers can scan and pay items directly on their phones and can even use the app to unlock the doors at night or whenever no support personnel is present. After a successful test run in April 2019, a rollout of this system to other locations in Switzerland is underway."
+    valoraP1="Avec is a convenience store brand created by Valora, a Swiss retail holding company operating mainly in German-speaking countries. Working with the Hackerbay development team, we created a mobile app for Avec X, the experimental, cashierless flagship store of the brand located in Zurich's Central Station.\n\nThe app enables a fully cashierless shopping experience. Customers can scan and pay items directly on their phones and can even use the app to unlock the doors at night or whenever no support personnel is present. After a successful test run in April 2019, a rollout of this system to other locations in Switzerland is underway.",
+    modulorH1 = "Modulor (2018)",
+    modulorP1 = "Modulor is a large arts supply retailer in Berlin, with a unique standing among artists and architects across the city. As a freelance UX consultant for their online division, I have helped optimize their current online retail offerings in several ways. Among many smaller upgrades, several iterations on the UX/UI of their online furniture configurator including a mobile-friendly version and improving the product detail view esp. on mobile devices were the most impactful. I also had an impact on the product design process itself: I advocated for the introduction of a living styleguide enabling the in-house graphic desiger to directly tweak the look and feel of UI elements in the store, moving the development process toward a more agile one.\n\nThese improvements were made possible by workshops with employees from all departments of the company; mapping out customer journeys as well as vigorous user- and A/B testing, extensive discussions with employees about and advocacy for a leaner product design process were all necessary in order to generate buy-in across the board."
+
 
     return <div>
         <Container>
@@ -50,6 +54,12 @@ function WorkPage() {
           paragraph={valoraP1}
           video={VideoAvec}
           poster={ImageAvec}
+        />
+        <WorkItem
+          className={pageStyles.Row}
+          headline={modulorH1}
+          paragraph={modulorP1}
+          image={ImageModulor}
         />
         <WorkItem
           className={pageStyles.Row}
